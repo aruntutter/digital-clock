82a5aee26fbcadabad984a9faab9eed51ba427b8
Arun Kumar R <aruntutter19@gmail.com>
2024-01-11 19:39:53 +0530

    rewritten the logic fully

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,43 @@
 "use strict";
 
-// global variables
+// Get elements
 
-// functions
-const init = () => {};
+let AMPM = document.getElementById("AMPM");
+let hours = document.getElementById("hours");
+let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
 
-//event listeners
+// Function
 
-// initial settings
-init();
+setInterval(() => {
+  let clock = new Date();
+  let hrs = clock.getHours();
+  let min = clock.getMinutes();
+  let sec = clock.getSeconds();
+
+  if (hrs > 12) {
+    hrs = hrs - 12;
+  }
+  if (hrs < 10) {
+    hrs = "0" + hrs;
+  }
+  if (min < 10) {
+    min = "0" + min;
+  }
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
+  if (hrs == 0) {
+    hrs = 12;
+  }
+
+  if (hrs < 12) {
+    AMPM.innerHTML = "PM";
+  } else {
+    AMPM.innerHTML = "AM";
+  }
+
+  hour.innerHTML = hrs;
+  minute.innerHTML = min;
+  second.innerHTML = sec;
+});
